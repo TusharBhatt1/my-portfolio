a61fb8c36de838eab38d05a4c4893d49d4cea050
Tushar Bhatt <tusharbhatt0135@gmail.com>
2023-05-08 13:32:43 +0530

    all

---
+++ src/Components/FrontPage.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import { Parallax } from 'react-parallax'
+import bg from "../Images/backgroundimage.jpg"
+import "./FrontPage.scss"
+import Typewriter from 'typewriter-effect'
+import logo from '../Images/logo512.png'
+import myself from "../Images/my.webp"
+import BorderColorIcon from '@mui/icons-material/BorderColor';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+export default function FrontPage() {
+  return (
+    <Parallax  strength={-600} bgImage={bg}>
+<div className='frontpage'>
+<div className='main'>
+<div className='leftside'>
+<div className='left1'>
+    Hey this is <br/> <h2>Tushar Bhatt</h2> 
+</div>
+<div className='left2'>
+<h3>
+<Typewriter options={{strings:"A React <br/>  Front End Developer",autoStart:true,loop:true}}/>
+</h3>
+<img className='img' height={"100vh"} width={"100vh"} src={logo}></img>
+</div>
+
+</div>
+<div className='rightside'>
+<img className='myself' src={myself}/>
+</div>
+
+</div>
+<footer className='footer'>
+<div>
+<p>Pursuing: Masters In Computer Application <BorderColorIcon/> </p> 
+</div>
+<div>
+<p><LocationOnIcon/> Dehradun, Uttarakhand , India</p>
+</div >
+</footer>
+</div>
+</Parallax>
+
+  )
+}
---
+++ src/Components/SecondPage.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Fade } from 'react-awesome-reveal'
+import coding from "../Images/coding.jpg"
+import DataObjectIcon from '@mui/icons-material/DataObject';
+import "./SecondPage.scss"
+
+export default function SecondPage() {
+  return (
+    <Fade className='maincontainer' delay={2000} style={{marginTop:"10vh"}} direction='left' duration={2000} >
+    <h3>
+      Projects
+    </h3>
+    <div className='projects'>
+       <div className='projectsone'>
+        <a href='https://myecommerce-pi.vercel.app/' target='blank'>React-Ecommerce App <DataObjectIcon/></a>
+        <a href='https://tusharbhatt1.github.io/MyMovieApp/' target='blank'>React-Movie App <DataObjectIcon/></a>
+        <a href='https://tusharbhatt1.github.io/Country_Guide_App/' target='blank'>JS-Country Guide <DataObjectIcon/></a>
+        </div>
+        <div className='projectstwo'>
+            <img height={"40%"} width={"40%"} src={coding}/>
+        </div>
+    </div>
+
+    </Fade>
+  )
+}
