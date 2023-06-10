4689e96fafed00c4e2c7c69344fee1016af143d4
Tushar Bhatt <tusharbhatt0135@gmail.com>
2023-06-10 23:16:04 +0530

    new project

--- src/Components/SecondPage.js
+++ src/Components/SecondPage.js
@@ -12,9 +12,10 @@ export default function SecondPage() {
     </h3>
     <div className='projects'>
        <div className='projectsone'>
-        <a href='https://myecommerce-pi.vercel.app/' target='blank'>React-Ecommerce App <DataObjectIcon/></a>
-        <a href='https://tusharbhatt1.github.io/MyMovieApp/' target='blank'>React-Movie App <DataObjectIcon/></a>
-        <a href='https://tusharbhatt1.github.io/Country_Guide_App/' target='blank'>JS-Country Guide <DataObjectIcon/></a>
+       <a href='https://cryptoweb-blond.vercel.app/' target='blank'>React - Cryto Buddy <DataObjectIcon/></a>
+        <a href='https://myecommerce-pi.vercel.app/' target='blank'>React - Ecommerce App <DataObjectIcon/></a>
+        <a href='https://tusharbhatt1.github.io/MyMovieApp/' target='blank'>React - Movie App <DataObjectIcon/></a>
+        <a href='https://tusharbhatt1.github.io/Country_Guide_App/' target='blank'>JS - Country Guide <DataObjectIcon/></a>
         </div>
         <div className='projectstwo'>
             <img height={"40%"} width={"40%"} src={coding}/>
