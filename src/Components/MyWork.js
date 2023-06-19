49773c966c1712fec183d63f0864e08109981d8c
Tushar Bhatt <tusharbhatt0135@gmail.com>
2023-06-19 23:31:33 +0530

    projectImages

--- src/Components/MyWork.js
+++ src/Components/MyWork.js
@@ -1,27 +1,45 @@
 import React from 'react'
 import { Fade } from 'react-awesome-reveal'
-import coding from "../Images/coding.jpg"
 import DataObjectIcon from '@mui/icons-material/DataObject';
 import "./MyWork.scss"
+import cryptobuddy from "../Images/CryptoBuddy.png"
+import ecommerce from "../Images/Ecommerce.png"
+import movie from "../Images/Movie.png"
+import countryGuide from "../Images/CountryGuide.png"
 
 export default function SecondPage() {
   return (
-    <Fade className='maincontainer' delay={1000}  direction='left' duration={1000} >
-    <h3>
+    <>
+    <h3 className='header'>
       My Work 
     </h3>
+    <Fade className='maincontainer' delay={1000}  direction='left' duration={1000} >
+    
     <div className='projects'>
        <div className='projectsone'>
-       <a href='https://cryptoweb-blond.vercel.app/' target='blank'>React - Cryto Buddy <DataObjectIcon/></a>
-        <a href='https://myecommerce-pi.vercel.app/' target='blank'>React - Ecommerce App <DataObjectIcon/></a>
-        <a href='https://tusharbhatt1.github.io/MyMovieApp/' target='blank'>React - Movie App <DataObjectIcon/></a>
+       <>
+       <img src={cryptobuddy} alt='project'/>
+       <a href='https://cryptoweb-blond.vercel.app/' target='blank'>React - Cryto Buddy</a>
+       </>
+       <>
+        <img src={ecommerce} alt='project'/>
+        <a href='https://myecommerce-pi.vercel.app/' target='blank'>React - Ecommerce </a>
+       </>
+       <>
+        <img src={movie} alt='project'/>
+        <a href='https://tusharbhatt1.github.io/MyMovieApp/' target='blank'>React - Movie App </a>
+       
+        </>
+        <>
+        <img src={countryGuide} alt='project'/>
+
         <a href='https://tusharbhatt1.github.io/Country_Guide_App/' target='blank'>JS - Country Guide <DataObjectIcon/></a>
+        </>
         </div>
-        <div className='projectstwo'>
-            <img height={"40%"} width={"40%"} src={coding}/>
-        </div>
+        
     </div>
 
     </Fade>
+    </>
   )
 }
