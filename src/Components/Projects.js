3b01875f4de82be8623e7020db64a1ae3a5b8ad5
Tushar Bhatt <tusharbhatt0135@gmail.com>
2023-07-20 09:52:19 +0530

    projects

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,3 +1,5 @@
+import {Netflix} from "../Images/Clone (3).png"
+import {ChatRoom} from "../Images/Clone (4).png"
 import NewEcommerce from "../Images/New-Ecommerce.png"
 import CryptoBuddy from "../Images/CryptoBuddy.png"
 import Ecommerce from "../Images/Ecommerce.png"
@@ -10,6 +12,18 @@ export default function Projects() {
 
         <div className="flex gap-12 flex-wrap justify-center items-center p-12 text-center font-bold font-serif ">
 
+            <div className="border-b-4 border-black p-2 rounded-lg hover:bg-blue-500 hover:text-white ">
+                 <a href="https://net-flix-7ewm.vercel.app/" target="_blank" rel="noreferrer">
+                <img src={Netflix} className="h-auto w-auto" alt="Netflix"/>
+                Netflix
+                </a>
+            </div>
+            <div className="border-b-4 border-black p-2 rounded-lg hover:bg-blue-500 hover:text-white ">
+                 <a href="https://chat-room-dc6ea.web.app/" target="_blank" rel="noreferrer">
+                <img src={ChatRoom} className="h-auto w-auto" alt="ChatRoom"/>
+                Chat Room
+                </a>
+            </div>
             <div className="border-b-4 border-black p-2 rounded-lg hover:bg-blue-500 hover:text-white ">
                  <a href="https://cryptoweb-blond.vercel.app/" target="_blank" rel="noreferrer">
                 <img src={CryptoBuddy} className="h-auto w-auto" alt="NewEcommerce"/>
