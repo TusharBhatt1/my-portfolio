55b2b10b1d7e8a565b8c23703609e49024899879
Tushar Bhatt <tusharbhatt0135@gmail.com>
2023-08-17 18:02:03 +0530

    time

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -25,21 +25,21 @@ export default function Projects() {
     
     
             <motion.div  initial={{opacity:0}} animate={{opacity:1}}
-        transition={{duration:2,delay:2}} class="wow slideInRight" data-wow-offset="10"  data-wow-iteration="10" className="border-b-4 border-black  rounded-lg hover:bg-blue-500 hover:text-white ">
+        transition={{duration:2,delay:4}} class="wow slideInRight" data-wow-offset="10"  data-wow-iteration="10" className="border-b-4 border-black  rounded-lg hover:bg-blue-500 hover:text-white ">
                  <a href="https://instagroww.vercel.app/" target="_blank" rel="noreferrer">
                 <img src={Instagroww} className="h-auto w-auto" alt="Instagroww"/>
                 InstaGroww - NextJS + Redux
                 </a>
             </motion.div>
             <motion.div  initial={{opacity:0}} animate={{opacity:1}}
-         transition={{duration:2,delay:2}} class="wow slideInRight" data-wow-offset="10"  data-wow-iteration="10" className="border-b-4 border-black  rounded-lg hover:bg-blue-500 hover:text-white ">
+         transition={{duration:2,delay:4}} class="wow slideInRight" data-wow-offset="10"  data-wow-iteration="10" className="border-b-4 border-black  rounded-lg hover:bg-blue-500 hover:text-white ">
                  <a href="https://net-flix-7ewm.vercel.app/" target="_blank" rel="noreferrer">
                 <img src={NetflixImage} className="h-auto w-auto" alt="Netflix"/>
                 Netflix
                 </a>
             </motion.div>
             <motion.div  initial={{opacity:0}} animate={{opacity:1}}
-         transition={{duration:2,delay:2}} className="border-b-4 border-black p-2 rounded-lg hover:bg-blue-500 hover:text-white ">
+         transition={{duration:2,delay:4}} className="border-b-4 border-black p-2 rounded-lg hover:bg-blue-500 hover:text-white ">
                  <a href="https://chat-room-dc6ea.web.app/" target="_blank" rel="noreferrer">
                 <img src={ChatRoomApp} className="h-auto w-auto" alt="ChatRoom"/>
                 Chat Room
@@ -47,21 +47,21 @@ export default function Projects() {
             </motion.div>
 
             <motion.div  initial={{opacity:0}} animate={{opacity:1}}
-              transition={{duration:2,delay:2}} className="border-b-4 border-black p-2 rounded-lg hover:bg-blue-500 hover:text-white ">
+              transition={{duration:2,delay:4}} className="border-b-4 border-black p-2 rounded-lg hover:bg-blue-500 hover:text-white ">
                  <a href="https://cryptoweb-blond.vercel.app/" target="_blank" rel="noreferrer">
                 <img src={CryptoBuddy} className="h-auto w-auto" alt="NewEcommerce"/>
                 React Crypto
                 </a>
             </motion.div>
             <motion.div  initial={{opacity:0}} animate={{opacity:1}}
-         transition={{duration:2,delay:2}} className="border-b-4 border-black p-2 rounded-lg hover:bg-blue-500 hover:text-white">
+         transition={{duration:2,delay:4}} className="border-b-4 border-black p-2 rounded-lg hover:bg-blue-500 hover:text-white">
             <a href="https://note-app-chi-liard.vercel.app/" target="_blank" rel="noreferrer">
                 <img src={NoteApp} className="" alt="NewEcommerce"/>
                Advanced Notes App
             </a>
             </motion.div>
             <motion.div  initial={{opacity:0}} animate={{opacity:1}}
-         transition={{duration:2,delay:2}}  className="border-b-4 border-black p-2 rounded-lg hover:bg-blue-500 hover:text-white">
+         transition={{duration:2,delay:4}}  className="border-b-4 border-black p-2 rounded-lg hover:bg-blue-500 hover:text-white">
             <a href="https://new-ecommerce-five.vercel.app/" target="_blank" rel="noreferrer">
                 <img src={NewEcommerce} className="" alt="NewEcommerce"/>
                React Ecommerce
@@ -69,7 +69,7 @@ export default function Projects() {
             </motion.div>
            
             <motion.div  initial={{opacity:0}} animate={{opacity:1}}
-         transition={{duration:2,delay:2}} className="border-b-4 border-black p-2 rounded-lg hover:bg-blue-500 hover:text-white">
+         transition={{duration:2,delay:4}} className="border-b-4 border-black p-2 rounded-lg hover:bg-blue-500 hover:text-white">
             <a href="https://myecommerce-pi.vercel.app/" target="_blank" rel="noreferrer">
                 <img src={Ecommerce} className="h-auto w-auto" alt="NewEcommerce"/>
                React Ecommerce</a>
