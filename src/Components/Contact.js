7b7658fa6025239de00f79e7689ef5d290a37b32
Tushar Bhatt <tusharbhatt0135@gmail.com>
2023-10-21 10:18:02 +0530

    resume

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -10,7 +10,7 @@ export default function Contact() {
   return (
     <div>
              <button style={{boxShadow:"3px 3px 3px  blue",padding:"10px",background:"black",borderRadius:"10px",display:"flex",letterSpacing:"2px",fontSize:"1.3rem",margin:"auto",marginTop:"25px",marginBottom:"25px"}}>
-             <a style={{fontFamily:"cursive",color:"white"}} href="https://lnkd.in/dJFh74MR" target='_blank' rel="noreferrer">My Resume</a>
+             <a style={{fontFamily:"cursive",color:"white"}} href="https://drive.google.com/file/d/15lY24sifwkbu2FeL1VfSLjHR2UwB9Z1d/view?usp=sharing" target='_blank' rel="noreferrer">My Resume</a>
              </button>  
 
       <p className='textShadow font-bold text-3xl p-2'>Open To</p>
